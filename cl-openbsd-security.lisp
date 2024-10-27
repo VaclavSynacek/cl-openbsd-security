acdd02e8eb1d2f8e9e2034c4063c1efd0d2f6ff7
Estevan Castilho (Tevo) <estevan.cps@gmail.com>
2024-10-27 22:15:51 +0000

    Remove call to QL:QUICKLOAD

--- cl-openbsd-security.lisp
+++ cl-openbsd-security.lisp
@@ -14,8 +14,6 @@
  | OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  |#
 
-(ql:quickload :cffi)
-
 (defpackage :cl-openbsd-security
   (:nicknames :openbsd :obsd)
   (:use :common-lisp :cffi)
